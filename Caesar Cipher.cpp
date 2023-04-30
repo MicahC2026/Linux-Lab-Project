db98d99b2064e75ed406fd49a8527133d4fe952f
MicahC2026 <132174555+MicahC2026@users.noreply.github.com>
2023-04-30 18:37:01 -0400

    Update Caesar Cipher.cpp

--- Caesar Cipher.cpp
+++ Caesar Cipher.cpp
@@ -1,4 +1,21 @@
 import sys
 
-alphabet = ['A
+alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+count = 0;
+x = int(sys.argv[1]);
+  
+for line in sys.stdin;
+  for letter in line;
+    letter = letter.upper();
+    if(letter in alphabet);
+      n_index = alphabet.index(letter) + x;
+      if(n_index > 25);
+        n_index = n_index - 26;
+      print(alphabet[n_index], end="");
+      count = count + 1;
+      if(count % 50 == 0);
+        print("\n", end="");
+      elif(count % 5 == 0);
+        print(" ", end="");
 
