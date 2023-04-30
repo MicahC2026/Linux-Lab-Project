ffd9767bb0df291a280c3ce0e520bae9a46494b7
MicahC2026 <132174555+MicahC2026@users.noreply.github.com>
2023-04-30 18:51:40 -0400

    Update Caesar Cipher.cpp

--- Caesar Cipher.cpp
+++ Caesar Cipher.cpp
@@ -1,26 +1,35 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cstdlib>
+
 using namespace std;
 
-int main()
-{
-  alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-
-  count = 0;
-  x = int(sys.argv[1]);
-  
-  for line in sys.stdin;
-    for letter in line;
-      letter = letter.upper();
-      if(letter in alphabet);
-        n_index = alphabet.index(letter) + x;
-        if(n_index > 25);
-          n_index = n_index - 26;
-        print(alphabet[n_index], end="");
-        count = count + 1;
-        if(count % 50 == 0);
-          print("\n", end="");
-        elif(count % 5 == 0);
-          print(" ", end="");
-  return 0;
+int main(int argc, char* argv[]) {
+    vector<char> alphabet = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+    int count = 0;
+    int x = atoi(argv[1]);
+    string line;
+    while (getline(cin, line)) {
+        for (char letter : line) {
+            letter = toupper(letter);
+            if (find(alphabet.begin(), alphabet.end(), letter) != alphabet.end()) {
+                int n_index = find(alphabet.begin(), alphabet.end(), letter) - alphabet.begin() + x;
+                if (n_index > 25) {
+                    n_index = n_index - 26;
+                }
+                cout << alphabet[n_index];
+                count++;
+                if (count % 50 == 0) {
+                    cout << endl;
+                }
+                else if (count % 5 == 0) {
+                    cout << " ";
+                }
+            }
+        }
+    }
+    return 0;
 }
 
