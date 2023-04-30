79a17b1be89205659f8f1ac046bee6e998bf87ab
MicahC2026 <132174555+MicahC2026@users.noreply.github.com>
2023-04-30 18:42:49 -0400

    Update Caesar Cipher.cpp

--- Caesar Cipher.cpp
+++ Caesar Cipher.cpp
@@ -1,21 +1,26 @@
-import sys
+#include <iostream>
+using namespace std;
 
-alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+int main()
+{
+  alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
-count = 0;
-x = int(sys.argv[1]);
+  count = 0;
+  x = int(sys.argv[1]);
   
-for line in sys.stdin;
-  for letter in line;
-    letter = letter.upper();
-    if(letter in alphabet);
-      n_index = alphabet.index(letter) + x;
-      if(n_index > 25);
-        n_index = n_index - 26;
-      print(alphabet[n_index], end="");
-      count = count + 1;
-      if(count % 50 == 0);
-        print("\n", end="");
-      elif(count % 5 == 0);
-        print(" ", end="");
+  for line in sys.stdin;
+    for letter in line;
+      letter = letter.upper();
+      if(letter in alphabet);
+        n_index = alphabet.index(letter) + x;
+        if(n_index > 25);
+          n_index = n_index - 26;
+        print(alphabet[n_index], end="");
+        count = count + 1;
+        if(count % 50 == 0);
+          print("\n", end="");
+        elif(count % 5 == 0);
+          print(" ", end="");
+  return 0;
+}
 
